81c6cfcd93219637cf4fbac156509ffa9895c4d9
Mert Yigittop <yigittopm@hotmail.com>
2020-12-06 21:29:09 +0300

    connect ok

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -1,19 +1,24 @@
 const router = require('express').Router();
 const Todo = require('../models/todo.model');
-let list = [];
 
 router.route('/add').post((req,res) => {
     const text = req.body.text;
     const date = req.body.date;
 
-    list.push({
+    const data = new Todo({
         text : text,
         date : date
-    });
+    })
+
+    data.save()
+        .then(res => res.json())
+        .catch(err => res.status(400).json(err));    
+
 })
 
 router.route('/').get((req,res) => {
-    res.send(list);
+    Todo.find()
+        .then(todos => res.json(todos))
 })
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const uri = process.env.ATLAS_URI;
-mongoose.connect("mongodb://localhost:27017", {useNewUrlParser: true,useCreateIndex: true, useUnifiedTopology: true});
+mongoose.connect(process.env.ATLAS_URI, {useNewUrlParser: true,useCreateIndex: true, useUnifiedTopology: true});
 
 const connection = mongoose.connection;
 connection.once('open', () => {
@@ -25,5 +24,5 @@ app.get('/', (req,res) => {
 app.use('/todos', require('./routes/todos.js'));
 
 app.listen(PORT, () => {
-    console.log("Serves listening on port: 5000");
+    console.log(`Serves listening on port: ${PORT}`);
 })
